348e501853529d8e6b53cf36de39f6cd6f49c228
igor <igor>
2024-06-17 15:27:29 +0300

    aa

--- index.js
+++ index.js
@@ -1,40 +1,26 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
-// We need this to build our post string
-var querystring = require('querystring');
-var http = require('https');
+const querystring = require('querystring');
+const http = require('https');
 
 function sendToBinadox(token, project) {
-    // Build the post string from an object
+    
     var post_data = querystring.stringify({
         'project': project
-    });
-
-    // An object of options to indicate where to post to
-    var post_options = {
-        host: 'httpbin.org',
-        port: '443',
-        path: '/post',
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/x-www-form-urlencoded',
-            'Authorization' : 'Bearer ' + token,
-            'Content-Length': Buffer.byteLength(post_data)
-        }
-    };
-    var post_req = http.request(post_options, function(res) {
-        res.setEncoding('utf8');
-        res.on('data', function (chunk) {
-            console.log('Response: ' + chunk);
-        });
-        res.on("error", function (error) {
-            console.error('ERROR :' + error.status);
-        });
     });
 
     // post the data
     post_req.write(post_data);
     post_req.end();
+
+    return fetch('https://httpbin.org/post',{
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded',
+            'Authorization' : 'Bearer ' + token,
+        },
+        body:post_data
+    })
 }
 
 try {
@@ -44,7 +30,16 @@ try {
   const time = (new Date()).toTimeString();
   core.setOutput("time", time);
 
-  sendToBinadox(nameToGreet, "XXXX");  
+
+
+  const res = await sendToBinadox(nameToGreet, "XXXX");
+  
+  if(res.ok){
+    const result =await res.json()
+    console.log('response', result)
+  } else{
+    console.log('ne ok response',res.status, res.statusText)
+  }
 
   // Get the JSON webhook payload for the event that triggered the workflow
   const payload = JSON.stringify(github.context.payload, undefined, 2)
