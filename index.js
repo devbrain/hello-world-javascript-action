5d30ee92a8a5797277ed695c5707481e024d62ee
igor <igor>
2024-06-17 15:45:43 +0300

    aabb

--- index.js
+++ index.js
@@ -1,43 +1,40 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
 const querystring = require('querystring');
-const http = require('https');
 
-async function sendToBinadox(token, project) {
-    
-    var post_data = querystring.stringify({
-        'project': project
-    });
-   
-    const res = await fetch('https://httpbin.org/post',{
+
+async function sendToBinadox(url, token, project) {
+  
+    const res = await fetch(url ,{
         method: 'POST',
         headers: {
             'Content-Type': 'application/x-www-form-urlencoded',
             'Authorization' : 'Bearer ' + token,
         },
-        body:post_data
+        body: querystring.stringify({
+            'project': project
+        })
     })
-
-    if(res.ok){
-        const result =await res.json()
-        console.log('response', result)
-      } else{
-        console.log('ne ok response',res.status, res.statusText)
-      }
+    return res
 }
 
 try {
   // `who-to-greet` input defined in action metadata file
-  const nameToGreet = core.getInput('who-to-greet');
+  const binadoxServerUrl = core.getInput('binadox-server-url');
+  const binadoxToken = core.getInput('binadox-secret-token');
+  const binadoxProject = core.getInput('binadox-project-name');
+
   console.log(`Hello ${nameToGreet}!`);
   const time = (new Date()).toTimeString();
   core.setOutput("time", time);
 
 
-
-  sendToBinadox(nameToGreet, "XXXX");
-  
-  
+  const res = sendToBinadox(binadoxServerUrl, binadoxToken, binadoxProject)  
+  if (res.ok) {
+    core.setOutput("status", "Request sent");
+  } else {
+    core.setFailed("Request failed: status = " + res.status + ", " + res.statusText);
+  }
 
   // Get the JSON webhook payload for the event that triggered the workflow
   const payload = JSON.stringify(github.context.payload, undefined, 2)
